fbb00480ec8f6986c373ece2a9e6bd68b7b7a69d
Micha Kühn <kuehn.michael@icloud.com>
2018-10-15 06:00:08 +0200

    better handling of cli usage errors + help output

--- app/bin/maps.php
+++ app/bin/maps.php
@@ -1,5 +1,7 @@
+#!/usr/bin/env php
 <?php
 declare(strict_types=1);
+
 
 use BreiteSeite\CodingChallenge\SynergistIO\Domain\Location\Resolver\Google\Geocode;
 use BreiteSeite\CodingChallenge\SynergistIO\Infrastructure\Google\Geocode\Client\JsonClient;
@@ -8,7 +10,43 @@ error_reporting(-1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$jsonClient = new JsonClient(getenv('APIKEY_GOOGLE_GEOCODING'));
-$geocode = new Geocode($jsonClient);
+const ENV_VAR_GOOGLE_GEOCODING_API_KEY = 'APIKEY_GOOGLE_GEOCODING';
+const EXIT_SUCCESS = 0;
+const EXIT_MISSING_ENV_CONFIG = 1;
+const EXIT_INVALID_USAGE = 2;
 
-var_dump($geocode->resolveLocation($argv[1])); die;
+$exitCode = (function (array $cliArguments): int {
+    $printUsage = function (): int {
+        echo 'Usage: maps.php <address>';
+        echo PHP_EOL;
+
+        return EXIT_INVALID_USAGE;
+    };
+
+    if (false === getenv(ENV_VAR_GOOGLE_GEOCODING_API_KEY)) {
+        echo 'Set env "APIKEY_GOOGLE_GEOCODING" with your API key to use this program';
+        echo PHP_EOL;
+
+        return EXIT_MISSING_ENV_CONFIG;
+    }
+
+    if (!isset($cliArguments[1])) {
+        return $printUsage();
+    }
+
+    if (count($cliArguments) > 2) {
+        return $printUsage();
+    }
+
+    $addressInput = $cliArguments[1];
+
+    $jsonClient = new JsonClient(getenv(ENV_VAR_GOOGLE_GEOCODING_API_KEY));
+    $geocode = new Geocode($jsonClient);
+
+
+    var_dump($geocode->resolveLocation($addressInput));
+
+    return EXIT_SUCCESS;
+})($argv);
+
+exit($exitCode);
