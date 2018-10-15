3b45f04bd9f5bceb1afafd4c94595c2d2ef00285
Micha Kühn <kuehn.michael@icloud.com>
2018-10-15 06:02:26 +0200

    remove possible unnecessary assignment of anonymous function

--- app/bin/maps.php
+++ app/bin/maps.php
@@ -2,7 +2,6 @@
 <?php
 declare(strict_types=1);
 
-
 use BreiteSeite\CodingChallenge\SynergistIO\Domain\Location\Resolver\Google\Geocode;
 use BreiteSeite\CodingChallenge\SynergistIO\Infrastructure\Google\Geocode\Client\JsonClient;
 
@@ -16,19 +15,19 @@ const EXIT_MISSING_ENV_CONFIG = 1;
 const EXIT_INVALID_USAGE = 2;
 
 $exitCode = (function (array $cliArguments): int {
-    $printUsage = function (): int {
-        echo 'Usage: maps.php <address>';
-        echo PHP_EOL;
-
-        return EXIT_INVALID_USAGE;
-    };
-
     if (false === getenv(ENV_VAR_GOOGLE_GEOCODING_API_KEY)) {
         echo 'Set env "APIKEY_GOOGLE_GEOCODING" with your API key to use this program';
         echo PHP_EOL;
 
         return EXIT_MISSING_ENV_CONFIG;
     }
+
+    $printUsage = function (): int {
+        echo 'Usage: maps.php <address>';
+        echo PHP_EOL;
+
+        return EXIT_INVALID_USAGE;
+    };
 
     if (!isset($cliArguments[1])) {
         return $printUsage();
